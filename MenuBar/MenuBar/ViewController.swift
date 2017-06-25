0799f10089cf6f22be6cff576cb8c29e5bf10349
Ian8829 <ghjkghj@naver.com>
2017-06-26 08:04:01 +0900

    Navigation Bars

--- MenuBar/MenuBar/ViewController.swift
+++ MenuBar/MenuBar/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func cameraPressed(_ sender: Any) {
+        
+        print("Camera Pressed")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
